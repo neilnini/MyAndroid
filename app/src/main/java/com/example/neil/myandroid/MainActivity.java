4d391383af11d69f51ac8cf11fc280d0babbbb3e
Neil Ni <monkeyni@gmail.com>
2015-07-03 16:16:50 +0800

    add a mark

--- app/src/main/java/com/example/neil/myandroid/MainActivity.java
+++ app/src/main/java/com/example/neil/myandroid/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //this is mark
         btn1 = (Button) findViewById(R.id.button1);
         btn2 = (Button) findViewById(R.id.button2);
         btn3 = (Button) findViewById(R.id.button3);
